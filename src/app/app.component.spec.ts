c3032ab5cc87a1962b18a959255ea64e7874d5a6
Tim Pambor <t.pambor@uniandes.edu.co>
2023-04-29 15:43:33 +0000

    Implement main page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,63 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'El aroma mágico'`, () => {
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('El aroma mágico');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
+  it(`should have a header with a h1 'El aroma mágico'`, () => {
+    const headers = fixture.debugElement.queryAll(By.css('header h1'));
+    expect(headers).toHaveSize(1);
+
+    const header = headers[0];
+    expect(header.nativeElement.textContent).toBe("El aroma mágico");
+  });
+
+  it(`should have a banner with an image of coffee`, () => {
+    const imgs = fixture.debugElement.queryAll(By.css('.row.hero img'));
+    expect(imgs).toHaveSize(1);
+
+    const img = imgs[0];
+
+    expect(img.nativeElement.getAttribute('src')).toBe("assets/image%201.png");
+    expect(img.nativeElement.getAttribute('alt')).toBe("café");
+  });
+
+  it(`should have a footer with contact information (phone, email, instagram)`, () => {
+    const footers = fixture.debugElement.queryAll(By.css('footer'));
+    expect(footers).toHaveSize(1);
+
+    const footer = footers[0];
+    expect(footer.nativeElement.textContent).toBe("Contact us: +57 3102105253 - info@elaromamagico.com - @elaromamagico");
+
+    const phone = footer.query(By.css('[data-testid="contact-phone"]'));
+    expect(phone.nativeElement.getAttribute('href')).toBe("tel:+573102105253");
+
+    const email = footer.query(By.css('[data-testid="contact-email"]'));
+    expect(email.nativeElement.getAttribute('href')).toBe("mailto:info@elaromamagico.com");
+
+    const instagram = footer.query(By.css('[data-testid="contact-instagram"]'));
+    expect(instagram.nativeElement.getAttribute('href')).toBe("https://www.instagram.com/elaromamagico/");
   });
 });
