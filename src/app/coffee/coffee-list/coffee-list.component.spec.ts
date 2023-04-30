9fdde5e474508428c545315520344905a8f3510c
Tim Pambor <t.pambor@uniandes.edu.co>
2023-04-30 11:00:05 +0000

    Add coffee-list component

---
+++ src/app/coffee/coffee-list/coffee-list.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { CoffeeListComponent } from './coffee-list.component';
+
+describe('CoffeeListComponent', () => {
+  let component: CoffeeListComponent;
+  let fixture: ComponentFixture<CoffeeListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CoffeeListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CoffeeListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/coffee/coffee.module.ts
+++ src/app/coffee/coffee.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  declarations: [
+    CoffeeListComponent
+  ],
+  exports: [
+    CoffeeListComponent
+  ]
 })
 export class CoffeeModule { }
